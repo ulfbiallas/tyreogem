5c61eb8b9bb9dd7a2ba7fae464a39819f02e4657
Ulf Biallas <ulfbiallas@googlemail.com>
2020-09-04 18:34:02 +0200

    add method to generate sphere mesh

--- src/main/java/de/ulfbiallas/tyreogem/mesh/MeshBuilder.java
+++ src/main/java/de/ulfbiallas/tyreogem/mesh/MeshBuilder.java
@@ -10,6 +10,51 @@ import de.ulfbiallas.tyreogem.core.math.Vec3d;
 
 public class MeshBuilder {
 
+    public static Mesh createSphere(double radius, int vSegments, int hSegments) {
+        int vn = vSegments < 3 ? 3 : vSegments;
+        int hn = hSegments < 2 ? 2 : hSegments;
+
+        final List<Vec3d> points = new ArrayList<>();
+        points.add(new Vec3d(0, -radius, 0)); // bottom vertex
+        for(int h=1; h<hn; ++h) {
+            double theta = - 0.5 * Math.PI + Math.PI * h / hn;
+            for(int k=0; k<vn; ++k) {
+                points.add(new Vec3d(Math.cos(theta) * Math.cos(k * 2 * Math.PI / vn) * radius, Math.sin(theta) * radius, Math.cos(theta) * Math.sin(k * 2 * Math.PI / vn) * radius));
+            }
+        }
+        points.add(new Vec3d(0, radius, 0)); // top vertex
+
+        final List<Face> faces = new ArrayList<>();
+        for(int h=0; h<hn; ++h) {
+            for(int k=0; k<vn; ++k) {
+                final int s0 = k;
+                final int s1 = k < vn-1 ? k+1 : 0;
+                if(h==0) {
+                    faces.add(new Face(Arrays.asList(
+                            new Vertex(0, null, null),
+                            new Vertex(1+s1, null, null),
+                            new Vertex(1+s0, null, null)
+                    )));
+                } else if(h==hn-1) {
+                    faces.add(new Face(Arrays.asList(
+                            new Vertex(1+(h-1)*vn+s0, null, null),
+                            new Vertex(1+(h-1)*vn+s1, null, null),
+                            new Vertex(points.size()-1, null, null)
+                    )));
+                } else {
+                    faces.add(new Face(Arrays.asList(
+                            new Vertex(1+(h-1)*vn+s0, null, null),
+                            new Vertex(1+(h-1)*vn+s1, null, null),
+                            new Vertex(1+h*vn+s1, null, null),
+                            new Vertex(1+h*vn+s0, null, null)
+                    )));
+                }
+            }
+        }
+
+        return new Mesh(points, faces);
+    }
+
     public static Mesh createCuboid(Vec3d position, Vec3d dimension) {
         final Vec3d r = dimension.scale(0.5);
         final List<Vec3d> offsets = Arrays.asList( //
