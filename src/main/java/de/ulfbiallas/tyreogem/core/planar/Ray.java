57d39e55dfcd04adbe851bfa93f19c6275cbdb3b
Ulf Biallas <ulfbiallas@googlemail.com>
2020-11-02 18:59:36 +0100

    add class for circle

--- src/main/java/de/ulfbiallas/tyreogem/core/planar/Ray.java
+++ src/main/java/de/ulfbiallas/tyreogem/core/planar/Ray.java
@@ -22,6 +22,10 @@ public class Ray {
         return direction;
     }
 
+    public Vec2d getPoint(double parameter) {
+        return origin.add(direction).scale(parameter);
+    }
+
     public RayIntersection intersect(Ray ray) {
         final Matrix2x2d matrix = new Matrix2x2d(direction, ray.getDirection().negate());
         final Vec2d r = ray.getOrigin().sub(origin);
