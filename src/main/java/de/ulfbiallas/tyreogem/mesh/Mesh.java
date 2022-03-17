5aa4e0e190d34f8d1b3bd82399f5a49f3da574b1
Ulf Biallas <ulfbiallas@googlemail.com>
2022-03-17 08:41:30 +0100

    extend mesh class

--- src/main/java/de/ulfbiallas/tyreogem/mesh/Mesh.java
+++ src/main/java/de/ulfbiallas/tyreogem/mesh/Mesh.java
@@ -1,10 +1,14 @@
 package de.ulfbiallas.tyreogem.mesh;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import de.ulfbiallas.tyreogem.core.math.Vec3d;
+import de.ulfbiallas.tyreogem.core.spatial.Triangle;
 
 public class Mesh {
 
@@ -12,9 +16,12 @@ public class Mesh {
 
     private final List<Face> faces;
 
+    private final Map<Material, List<Face>> facesByMaterial = new HashMap<>(); // Faces without materials have the key null.
+
     public Mesh(List<Vec3d> points, List<Face> faces) {
         this.points = points;
         this.faces = faces;
+        groupFacesByMaterial();
     }
 
     public Mesh(List<Mesh> meshes) {
@@ -26,6 +33,7 @@ public class Mesh {
             this.points.addAll(mesh.getPoints());
             this.faces.addAll(mesh.faces.stream().map(face -> new Face(face.getVertices().stream().map(vertex -> new Vertex(vertex.getPointIndex()+pointOffset, vertex.getNormal(), vertex.getTextureCoordinates())).collect(Collectors.toList()), face.getMaterial())).collect(Collectors.toList()));
         }
+        groupFacesByMaterial();
     }
 
     public List<Vec3d> getPoints() {
@@ -58,6 +66,84 @@ public class Mesh {
         final Vec3d b = v2.sub(v0);
 
         return a.cross(b).normalize();
+    }
+
+    public Mesh triangulateByEarClipping() {
+        final List<Face> triangles = new ArrayList<>();
+        for(Face face: faces) {
+            int vertexCount = face.getVertices().size();
+            if(vertexCount < 3) {
+                // Invalid face. Will be discarded
+            } else if(vertexCount == 3) {
+                triangles.add(face);
+            } else {
+                List<Vertex> vertices = face.getVertices().stream().collect(Collectors.toList());
+                while(vertices.size() >= 3) {
+                    int earOffset = getEarOffset(vertices);
+                    triangles.add(new Face(
+                        Arrays.asList(
+                            vertices.get((earOffset + 0) % vertices.size()),
+                            vertices.get((earOffset + 1) % vertices.size()),
+                            vertices.get((earOffset + 2) % vertices.size())
+                        ),   
+                        face.getMaterial()
+                    ));
+                    vertices.remove((earOffset + 1) % vertices.size()); // clip ear
+                }
+            }
+        }
+        return new Mesh(this.points, triangles);
+    }
+
+    public Map<Material, List<Face>> getFacesByMaterial() {
+        return facesByMaterial;
+    }
+
+    public List<Material> getMaterials() {
+        final List<Material> availableMaterials = facesByMaterial.keySet().stream().collect(Collectors.toList());
+        return availableMaterials;
+    }
+
+    // Returns the first triangle that does not contain any of the other vertices
+    private int getEarOffset(List<Vertex> vertices) {
+        int offset = 0;
+        Triangle triangle = getTriangleAtOffset(vertices, offset);
+        while(containsOtherPoints(triangle, vertices, offset)) {
+            offset++;
+            triangle = getTriangleAtOffset(vertices, offset);
+        }
+        return offset;
+    }
+
+    private boolean containsOtherPoints(Triangle triangle, List<Vertex> vertices, int offset) {
+        for(int k=0; k<offset; ++k) {
+            if(triangle.isPointInside(this.points.get(vertices.get(k).getPointIndex()))) {
+                return true;
+            }
+        }
+        for(int k=offset+3; k<vertices.size(); ++k) {
+            if(triangle.isPointInside(this.points.get(vertices.get(k).getPointIndex()))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Triangle getTriangleAtOffset(List<Vertex> vertices, int offset) {
+        Vec3d p1 = this.points.get(vertices.get((offset + 0) % vertices.size()).getPointIndex());
+        Vec3d p2 = this.points.get(vertices.get((offset + 1) % vertices.size()).getPointIndex());
+        Vec3d p3 = this.points.get(vertices.get((offset + 2) % vertices.size()).getPointIndex());
+        return new Triangle(p1, p2, p3);
+    }
+
+    private void groupFacesByMaterial() {
+        for(Face face: getFaces()) {
+            final Material material = face.getMaterial();
+            if(!facesByMaterial.containsKey(material)) {
+                facesByMaterial.put(material, new ArrayList<Face>());
+            }
+            facesByMaterial.get(material).add(face);
+        }
     }
 
 }
