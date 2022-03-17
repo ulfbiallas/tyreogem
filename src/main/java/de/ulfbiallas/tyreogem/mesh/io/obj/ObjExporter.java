d73179d5446a9683919b86fd7fbc476635d57930
Ulf Biallas <ulfbiallas@googlemail.com>
2022-03-17 22:07:09 +0100

    support mesh without material in obj

--- src/main/java/de/ulfbiallas/tyreogem/mesh/io/obj/ObjExporter.java
+++ src/main/java/de/ulfbiallas/tyreogem/mesh/io/obj/ObjExporter.java
@@ -52,7 +52,11 @@ public class ObjExporter implements Exporter {
     }
 
     private String createMtlLine(String name, String value) {
-        return name + " " + value +"\n";
+        if(value != null) {
+            return name + " " + value +"\n";
+        } else {
+            return "";
+        }
     }
 
     public void writeMtlFile(ObjMesh objMesh, Writer writer) throws IOException {
