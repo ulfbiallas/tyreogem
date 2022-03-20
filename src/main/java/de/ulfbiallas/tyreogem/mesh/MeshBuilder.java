ae2d1bc3cfcd3549db687bcfb46b3d33ca3d438a
Ulf Biallas <ulfbiallas@googlemail.com>
2022-03-20 13:07:46 +0100

    add normals and uv for sphere in mesh builder

--- src/main/java/de/ulfbiallas/tyreogem/mesh/MeshBuilder.java
+++ src/main/java/de/ulfbiallas/tyreogem/mesh/MeshBuilder.java
@@ -20,14 +20,19 @@ public class MeshBuilder {
         int hn = hSegments < 2 ? 2 : hSegments;
 
         final List<Vec3d> pointsOffsets = new ArrayList<>();
+        final List<Vec2d> uvs = new ArrayList<>();
         pointsOffsets.add(new Vec3d(0, -radius, 0)); // bottom vertex
+        uvs.add(new Vec2d(0, 0));
         for(int h=1; h<hn; ++h) {
-            double theta = - 0.5 * Math.PI + Math.PI * h / hn;
+            double theta = -0.5 * Math.PI + Math.PI * h / hn;
             for(int k=0; k<vn; ++k) {
-                pointsOffsets.add(new Vec3d(Math.cos(theta) * Math.cos(k * 2 * Math.PI / vn) * radius, Math.sin(theta) * radius, Math.cos(theta) * Math.sin(k * 2 * Math.PI / vn) * radius));
+                double phi = k * 2 * Math.PI / vn;
+                pointsOffsets.add(new Vec3d(Math.cos(theta) * Math.cos(phi) * radius, Math.sin(theta) * radius, Math.cos(theta) * Math.sin(phi) * radius));
+                uvs.add(new Vec2d(1.0 * k / vn, 1.0 * h / hn));
             }
         }
         pointsOffsets.add(new Vec3d(0, radius, 0)); // top vertex
+        uvs.add(new Vec2d(0, 1.0));
 
         final List<Vec3d> points = pointsOffsets.stream().map(offset -> position.add(offset)).collect(Collectors.toList());
 
@@ -36,27 +41,49 @@ public class MeshBuilder {
             for(int k=0; k<vn; ++k) {
                 final int s0 = k;
                 final int s1 = k < vn-1 ? k+1 : 0;
+                Vec3d n0, n1, n2, n3;
+                Vec2d uv0, uv1, uv2, uv3;
                 if(h==0) {
                     // bottom triangles
+                    n0 = pointsOffsets.get(1+s0).normalize();
+                    n1 = pointsOffsets.get(1+s1).normalize();
+                    n2 = pointsOffsets.get(0).normalize();
+                    uv0 = uvs.get(1+s0);
+                    uv1 = new Vec2d(uv0.x + 1.0 / vn, uv0.y);
+                    uv2 = new Vec2d(0.5 * (uv0.x + uv1.x), 0);
                     faces.add(new Face(Arrays.asList(
-                            new Vertex(1+s0, null, null),
-                            new Vertex(1+s1, null, null),
-                            new Vertex(0, null, null)
+                            new Vertex(1+s0, n0, uv0),
+                            new Vertex(1+s1, n1, uv1),
+                            new Vertex(0,    n2, uv2)
                     ), material));
                 } else if(h==hn-1) {
-                    // body quads
+                    // top triangles
+                    n0 = pointsOffsets.get(points.size()-1).normalize();
+                    n1 = pointsOffsets.get(1+(h-1)*vn+s1).normalize();
+                    n2 = pointsOffsets.get(1+(h-1)*vn+s0).normalize();
+                    uv2 = uvs.get(1+(h-1)*vn+s0);
+                    uv1 = new Vec2d(uv2.x + 1.0 / vn, uv2.y);
+                    uv0 = new Vec2d(0.5 * (uv2.x + uv1.x), 1.0);
                     faces.add(new Face(Arrays.asList(
-                            new Vertex(points.size()-1, null, null),
-                            new Vertex(1+(h-1)*vn+s1, null, null),
-                            new Vertex(1+(h-1)*vn+s0, null, null)
+                            new Vertex(points.size()-1, n0, uv0),
+                            new Vertex(1+(h-1)*vn+s1,   n1, uv1),
+                            new Vertex(1+(h-1)*vn+s0,   n2, uv2)
                     ), material));
                 } else {
-                    // top triangles
+                    // body quads
+                    n0 = pointsOffsets.get(1+h*vn+s0).normalize();
+                    n1 = pointsOffsets.get(1+h*vn+s1).normalize();
+                    n2 = pointsOffsets.get(1+(h-1)*vn+s1).normalize();
+                    n3 = pointsOffsets.get(1+(h-1)*vn+s0).normalize();
+                    uv0 = uvs.get(1+h*vn+s0);
+                    uv1 = new Vec2d(uv0.x + 1.0 / vn, uv0.y);
+                    uv3 = uvs.get(1+(h-1)*vn+s0);
+                    uv2 = new Vec2d(uv3.x + 1.0 / vn, uv3.y);
                     faces.add(new Face(Arrays.asList(
-                            new Vertex(1+h*vn+s0, null, null),
-                            new Vertex(1+h*vn+s1, null, null),
-                            new Vertex(1+(h-1)*vn+s1, null, null),
-                            new Vertex(1+(h-1)*vn+s0, null, null)
+                            new Vertex(1+h*vn+s0,     n0, uv0),
+                            new Vertex(1+h*vn+s1,     n1, uv1),
+                            new Vertex(1+(h-1)*vn+s1, n2, uv2),
+                            new Vertex(1+(h-1)*vn+s0, n3, uv3)
                     ), material));
                 }
             }
